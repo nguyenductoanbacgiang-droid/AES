c525ad0b4d975d1e19ec3011d847735f7ab98ed1
toan <nguyenductoanbacgiang@gmail.com>
2025-11-29 17:31:56 +0700

    Add files via upload

---
+++ main(d).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "aes_decrypt.h"
+#include "keyexpansion.h"
+
+int main(){
+    uint8_t ciphertext[16] = {0x69,0xC4,0xE0,0xD8,0x6A,0x7B,0x04,0x30,
+                               0xd8,0xCD,0xB7,0x80,0x70,0xB4,0xC5,0x5A};
+    uint8_t key[16] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
+                        0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F};
+    uint8_t roundKeys[176];
+    KeyExpansion(key,roundKeys);
+    AES_Decrypt(ciphertext,roundKeys);
+    printf("Plaintext:\n");
+    int i;
+    for(i=0;i<16;i++) printf("%02x ",ciphertext[i]);
+    printf("\n");
+    return 0;
+}
+
